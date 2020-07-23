9cf0f5e90d92b59b0f7fdb931c5ee4f881dfb083
oren4 <leronmenezes@gmail.com>
2020-07-23 09:59:57 +0530

    creating a test python script to extract the table

---
+++ test_html_parsing.py
@@ -0,0 +1,37 @@
+"""
+Created on Thu Jul 27 2020
+
+@author: Leron
+"""
+
+#import libraries
+import urllib.request, urllib.parse,urllib.error
+from bs4 import BeautifulSoup
+
+# pulling the html
+url = 'https://en.wikipedia.org/wiki/List_of_postal_codes_of_Canada:_M'
+html = urllib.request.urlopen(url).read()
+soup = BeautifulSoup(html,'html.parser')
+
+#Retrieving data from the html
+table = soup.find('table',attrs = { 'class' : 'wikitable sortable jquery-tablesorter'})
+table_body = soup.find('tbody')
+rows = table_body.find_all('tr')
+
+#print('These are rows',rows)
+
+#now we create dictionaries for the data from the table
+postal_code = []
+borough = []
+neighbourhood = []
+dict_df = {'Postal Code': postal_code, 'Borough': borough, 'Neighbourhoods': neighbourhood}
+
+for row in rows:
+    for i in range(1):
+        postal_code.append(row.find('td'))
+
+        for
+
+        borough.append(row.find('td'))
+        neighbourhood.append(row.find('td'))
+print(dict_df)
