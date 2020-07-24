a365b7e4f392efb8422726a7c8812cad3603bb5d
oren4 <leronmenezes@gmail.com>
2020-07-24 10:06:25 +0530

    tried parsing using get_text, still WIP

--- test_html_parsing.py
+++ test_html_parsing.py
@@ -17,8 +17,16 @@ soup = BeautifulSoup(html,'html.parser')
 table = soup.find('table',attrs = { 'class' : 'wikitable sortable jquery-tablesorter'})
 table_body = soup.find('tbody')
 rows = table_body.find_all('tr')
-
 #print('These are rows',rows)
+for row in rows:
+    row1 = row.find('td')
+    print(row1.td.get_text())
+#print(soup.tr.get_text())
+
+#with open('Table.txt','w') as File1:
+#    for row in rows:
+#        File1.write(str(row))
+
 
 #now we create dictionaries for the data from the table
 postal_code = []
@@ -26,9 +34,13 @@ borough = []
 neighbourhood = []
 dict_df = {'Postal Code': postal_code, 'Borough': borough, 'Neighbourhoods': neighbourhood}
 
-for row in rows:
-    for i in range(1):
-        postal_code.append(row.find('td'))
-        borough.append(row.find('td'))
-        neighbourhood.append(row.find('td'))
-print(dict_df)
+List_op_rows = []
+
+#for row in rows:
+#    List_op_rows.append(row.find_all('tr'))
+
+    #for i in range(1):
+    #    postal_code.append(row.find('td'))
+    #    borough.append(row.find('td'))
+    #    neighbourhood.append(row.find('td'))
+#print(List_op_rows)
